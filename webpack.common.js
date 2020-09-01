f5aa9f3777edd880be33fe68a70be45c6a30cf2d
Layo Folaranmi <thedarkkid.codes@gmail.com>
2020-09-01 16:59:31 +0400

    Add html-loader, file-loader and clean-webpack plugin

--- webpack.common.js
+++ webpack.common.js
@@ -16,6 +16,20 @@ module.exports = {
                     'css-loader', //2. Turns css into commonjs
                     'sass-loader' //1. Turns sass into css
                 ]
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },{
+                test: /\.(svg|png|jpeg|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        outputPath: "imgs"
+                    }
+                }
+
             }
         ]
     }
