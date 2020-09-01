002b4ec54393c1a94f5803edbd82f7cbdc08bdaa
Layo Folaranmi <thedarkkid.codes@gmail.com>
2020-09-01 16:16:35 +0400

    added dev & prod configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
 
 module.exports = {
     entry: "./src/index.js",
-    mode: "development",
-    output: {
-        filename: "index.[contentHash].js",
-        path: path.resolve(__dirname, "dist")
-    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "src/static/template.html"
