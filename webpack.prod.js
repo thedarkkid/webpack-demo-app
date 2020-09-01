6efb249a3e465a10534df9a7508ce1218a3c3691
Layo Folaranmi <thedarkkid.codes@gmail.com>
2020-09-01 18:02:33 +0400

    finished tut

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,9 @@ const path = require("path");
 const common = require("./webpack.common");
 const {merge} = require("webpack-merge");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
@@ -9,6 +12,27 @@ module.exports = merge(common, {
         filename: "[name].[contentHash].bundle.js",
         path: path.resolve(__dirname, "dist")
     },
-    plugins: [new CleanWebpackPlugin()]
+    optimization:{
+        minimizer: [
+             new OptimizeCssAssetPlugin(),
+            new TerserPlugin()
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({filename: "[name].[contentHash].css"}),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, //3. Inject styles into DOM
+                    'css-loader', //2. Turns css into commonjs
+                    'sass-loader' //1. Turns sass into css
+                ]
+            }
+        ]
+    }
 });
 
